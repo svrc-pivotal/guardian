f8cdc182c4b87410e415591fccf9ca8f0e1a6cfc
George Lestaris <glestaris@pivotal.io>
2016-04-07 17:05:45 +0100

    wip: manually testing runc logs forwarding
    Signed-off-by: Petar Petrov <pppepito86@gmail.com>

---
+++ gqt/logging_test.go
@@ -0,0 +1,35 @@
+package gqt_test
+
+import (
+	"github.com/cloudfoundry-incubator/garden"
+	"github.com/cloudfoundry-incubator/guardian/gqt/runner"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+)
+
+var _ = FDescribe("Logging", func() {
+	var (
+		client        *runner.RunningGarden
+		container     garden.Container
+		containerSpec garden.ContainerSpec
+	)
+
+	BeforeEach(func() {
+		containerSpec = garden.ContainerSpec{
+			RootFSPath: runner.RootFSPath,
+		}
+	})
+
+	JustBeforeEach(func() {
+		var err error
+		client = startGarden()
+		container, err = client.Create(containerSpec)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("forwards RunC logs", func() {
+		Eventually(client, "1s").Should(gbytes.Say("guardian-runc-logging-test-start"))
+	})
+})
