591b95766617da676a87912b1e9a5139e0f69200
Julz Friedman <julz.friedman@uk.ibm.com>
2016-05-11 18:38:02 +0300

    wip
    Signed-off-by: Georgi Sabev <georgethebeatle@gmail.com>

--- cmd/thesecret-garden/main_linux.go
+++ cmd/thesecret-garden/main_linux.go
@@ -3,8 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 )
 
 func main() {
-	fmt.Print(os.Args[1])
+	_, err := exec.Command("mount", "--make-slave", os.Args[1]).CombinedOutput()
+	if err != nil {
+		fmt.Printf("ERR: %s\n", err.Error())
+	}
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdout = os.Stdout
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("ERR: %s\n", err.Error())
+	}
 }
--- cmd/thesecret-garden/thesecret_garden_linux_test.go
+++ cmd/thesecret-garden/thesecret_garden_linux_test.go
@@ -7,60 +7,115 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 )
 
 var _ = FDescribe("The Secret Garden", func() {
-	const script = `
-	  echo $DATA_DIR
+	const makeSecretMount = `#!/bin/sh
+		set -e -x
+		mkdir -p ${DATA_DIR}
+		mount -t tmpfs tmpfs ${DATA_DIR}
+		touch ${DATA_DIR}/mysecret
+		echo -n password > ${DATA_DIR}/mysecret
 	`
-	// set -e -x
-	// echo ${DATA_DIR}
-	// mkdir -p ${DATA_DIR}
-	// mount -t tmpfs tmpfs ${DATA_DIR}
-	// touch ${DATA_DIR}/mysecret
-	// echo -n password > ${DATA_DIR}/mysecret
+	const accessSharedMount = `#!/bin/sh
+		set -e -x
+		sleep 1
+		stat ${PUBLIC_DIR}/myfile
+	`
 
 	var (
+		stubProcess string
 		fakeDataDir string
 		err         error
 		session     *gexec.Session
 	)
 
-	unshareCmd := func(program string, args ...string) *os.Process {
+	unshareCmd := func(program string, args ...string) *gexec.Session {
 		cmd := exec.Command("unshare", "-m", "/bin/bash")
 		cmd.Env = []string{
-			"PATH=/bin:/usr/bin",
+			"PATH=/bin:/usr/bin:.",
 			fmt.Sprintf("DATA_DIR=%s", filepath.Join(fakeDataDir, "secret")),
+			fmt.Sprintf("PUBLIC_DIR=%s", filepath.Join(fakeDataDir, "shared")),
 		}
 
-		stdinPipe, _ := cmd.StdinPipe()
+		stdinPipe, err := cmd.StdinPipe()
+		Expect(err).NotTo(HaveOccurred())
+
 		bw := bufio.NewWriter(stdinPipe)
-		bw.WriteString(fmt.Sprintf("%s %s", program, args...))
+		bw.WriteString(fmt.Sprintf("%s %s\n", program, strings.Join(args, " ")))
 		bw.Flush()
 		stdinPipe.Close()
 
-		err = cmd.Start()
-		Expect(err).ToNot(HaveOccurred())
+		sess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 
-		go func() {
-			err = cmd.Wait()
-			Expect(err).ToNot(HaveOccurred())
-		}()
+		Expect(err).NotTo(HaveOccurred())
 
-		return cmd.Process
+		return sess
 	}
 
 	BeforeEach(func() {
-		fakeDataDir, err = ioutil.TempDir("", "")
+		fakeDataDir, err = ioutil.TempDir("", "data")
 		Expect(err).NotTo(HaveOccurred())
-		unshareCmd(theSecretGardenBin, "/var/vcap/data", script)
+
+		Expect(exec.Command("mount", "--bind", fakeDataDir, fakeDataDir).Run()).To(Succeed())
+		Expect(exec.Command("mount", "--make-shared", fakeDataDir).Run()).To(Succeed())
 	})
 
-	It("", func() {
-		Eventually(session).Should(Succeed())
+	AfterEach(func() {
+		Expect(exec.Command("umount", fakeDataDir).Run()).To(Succeed())
+		Expect(os.RemoveAll(fakeDataDir)).To(Succeed())
 	})
+
+	Context("when the process creates a mount", func() {
+		BeforeEach(func() {
+			stubProcess = "create-mount.sh"
+
+			Expect(ioutil.WriteFile(stubProcess, []byte(makeSecretMount), 0777)).To(Succeed())
+		})
+
+		AfterEach(func() {
+			Expect(os.Remove(stubProcess)).To(Succeed())
+		})
+
+		It("prevents the mount to be seen from outside the namespace", func() {
+			session = unshareCmd(theSecretGardenBin, fakeDataDir, stubProcess)
+
+			Consistently(func() []os.FileInfo {
+				fileInfo, _ := ioutil.ReadDir(filepath.Join(fakeDataDir, "secret"))
+				return fileInfo
+			}, "5s").Should(BeEmpty())
+
+			Eventually(session, "5s").Should(gexec.Exit(0))
+		})
+	})
+
+	Context("when a mount is created outside the namespace", func() {
+		BeforeEach(func() {
+			stubProcess = "access-mount.sh"
+			Expect(ioutil.WriteFile(stubProcess, []byte(accessSharedMount), 0777)).To(Succeed())
+		})
+
+		AfterEach(func() {
+			Expect(exec.Command("umount", filepath.Join(fakeDataDir, "shared")).Run()).To(Succeed())
+			Expect(os.Remove(stubProcess)).To(Succeed())
+		})
+
+		It("is visible inside the unshared namespace", func() {
+			session = unshareCmd(theSecretGardenBin, fakeDataDir, stubProcess)
+
+			Expect(exec.Command("mkdir", filepath.Join(fakeDataDir, "shared")).Run()).To(Succeed())
+			Expect(exec.Command("mount", "-t", "tmpfs", "tmpfs", filepath.Join(fakeDataDir, "shared")).Run()).To(Succeed())
+			Expect(exec.Command("touch", filepath.Join(fakeDataDir, "shared", "myfile")).Run()).To(Succeed())
+
+			Eventually(session, "5s").Should(gexec.Exit(0))
+			Expect(session.Out).To(gbytes.Say("shared/myfile"))
+		})
+	})
+
 })
